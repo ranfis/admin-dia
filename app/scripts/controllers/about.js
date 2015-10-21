a8ec80a229b6eddae08ecca0732c4d0db5d9f817
Ranfis S <ranfis.sh@gmail.com>
2015-10-21 17:43:51 -0400

    Initial Project generated with yo

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name diaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the diaApp
+ */
+angular.module('diaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name diaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the diaApp
+ */
+angular.module('diaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
