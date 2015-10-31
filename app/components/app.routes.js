66f19da148bb5813b0ab849eb4dbf04950d9e4c9
Ranfis S <ranfis.sh@gmail.com>
2015-10-30 23:19:12 -0400

    Remove componenets routes from app.routes

--- app/components/app.routes.js
+++ app/components/app.routes.js
@@ -2,38 +2,12 @@
 
 angular
   .module('diaApp')
-  .config(function ($routeProvider,USER_ROLES) {
+  .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'components/main.html',
         controller: 'MasterCtrl',
         controllerAs: 'main'
-      })
-      .when('/login', {
-        templateUrl: 'components/login/login.html',
-        controller: 'MasterCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'components/about/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/tables', {
-        templateUrl: 'components/tables.html',
-        controller: 'MasterCtrl',
-        controllerAs: 'master',
-        data: {
-          authorizedRoles: [USER_ROLES.admin, USER_ROLES.editor]
-        }
-      })
-      .when('/dashboard', {
-        templateUrl: 'components/dashboard/dashboard.html',
-        controller: 'MasterCtrl',
-        controllerAs: 'master',
-        data: {
-          authorizedRoles: [USER_ROLES.admin]
-        }
       })
       .otherwise({
         redirectTo: '/'
