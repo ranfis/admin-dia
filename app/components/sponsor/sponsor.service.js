c33d72a58641a53307c06394ddca0dfd765d0443
Ranfis S <ranfis.sh@gmail.com>
2015-11-01 00:42:00 -0400

    Create sponsors component

---
+++ app/components/sponsor/sponsor.service.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('diaApp')
+  .service('SponsorService', function ($http, WS, REQUEST){
+
+    this.sponsors = [];
+
+    this.getAll = function (sessionId) {
+      var params = {
+        params:{
+          session_id: sessionId
+        }
+      };
+      return $http
+        .get(WS+"/sponsor/list", params)
+        .then(function(res) {
+          return res.data.result;
+        }, function(err){
+          console.error(err);
+        });
+    };
+
+    this.create = function(sponsor){
+      return $http
+        .post(WS+"/sponsor/add", sponsor,REQUEST.PLAIN)
+        .then(function (res) {
+          return (res.data.code === 0);
+        }, function(err){
+          console.error(err);
+        });
+    };
+
+    this.update = function(sponsor){
+      console.log("update",sponsor);
+      return $http
+        .put(WS+"/sponsor/update", sponsor,REQUEST.PLAIN)
+        .then(function (res) {
+          console.log("http",res);
+          return (res.data.code === 0);
+        },function (err){
+          console.error("http",err);
+        });
+    };
+  });
