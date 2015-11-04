c8f788db38de55bdb8b49896b45bcd37595230d3
Ranfis S <ranfis.sh@gmail.com>
2015-11-03 23:14:37 -0400

    Update auth

--- app/components/login/login.controller.js
+++ app/components/login/login.controller.js
@@ -1,16 +1,23 @@
 'use strict';
 
 angular.module('diaApp')
-  .controller('LoginCtrl', function ($scope, $rootScope, AUTH_EVENTS, AuthService) {
-    $scope.credentials = {};
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/login', {
+        templateUrl: 'components/login/login.html',
+        controller: function ($scope, $rootScope, $location, AUTH_EVENTS, AuthService) {
+          $scope.credentials = {};
 
-    $scope.authenticate = function(credentials) {
-      AuthService.login(credentials)
-        .then(function (user) {
-          $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
-          $scope.setCurrentUser(user);
-        },function () {
-          $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+          $scope.authenticate = function (credentials) {
+            AuthService.login(credentials)
+              .then(function (user) {
+                $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+                $scope.setCurrentUser(user);
+                $location.path( "/" );
+              }, function () {
+                $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+              });
+          };
+        }
       });
-    };
   });
--- app/components/login/session.service.js
+++ app/components/login/session.service.js
@@ -3,10 +3,9 @@
 angular.module('diaApp')
   .service('Session', function () {
     this.create = function (user) {
-      //console.log("Session create",user);
       this.id = user.sessionId;
       this.userEmail = user.email;
-      this.userRole = user.role.id;
+      this.userRole = user.role.name;
     };
     this.destroy = function () {
       this.id = null;
