c2015dc6a923ef5aa57a6f74918f233ddb6a330a
Ranfis S <ranfis.sh@gmail.com>
2015-11-14 15:25:46 -0400

    CleanUp

--- app/shared/constants.js
+++ app/shared/constants.js
@@ -3,12 +3,12 @@
 angular
   .module('diaApp')
   .constant('AUTH_EVENTS', {
-  loginSuccess: 'auth-login-success',
-  loginFailed: 'auth-login-failed',
-  logoutSuccess: 'auth-logout-success',
-  sessionTimeout: 'auth-session-timeout',
-  notAuthenticated: 'auth-not-authenticated',
-  notAuthorized: 'auth-not-authorized'
+    loginSuccess: 'auth-login-success',
+    loginFailed: 'auth-login-failed',
+    logoutSuccess: 'auth-logout-success',
+    sessionTimeout: 'auth-session-timeout',
+    notAuthenticated: 'auth-not-authenticated',
+    notAuthorized: 'auth-not-authorized'
 })
   .constant('USER_ROLES', {
     all: '*',
@@ -18,4 +18,4 @@ angular
   .constant("WS","http://localhost/ws-dia")
   .constant("REQUEST",{
     PLAIN: {headers: {'Content-Type': "text/plain"}}
-  })
+  });
