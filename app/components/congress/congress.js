12e1a5fbad2c1dcbe72ed572f40651c33be0810c
Ranfis S <ranfis.sh@gmail.com>
2015-11-14 16:19:32 -0400

    WIP: Congress update and delete

--- app/components/congress/congress.js
+++ app/components/congress/congress.js
@@ -3,38 +3,75 @@
 angular.module('diaApp')
   .config(function ($routeProvider) {
 
-    var CongressListCtrl = function ($scope, Session,CongressService) {
+    var CongressesListCtrl = function ($scope, Session, CongressService) {
       CongressService.getAll(Session.id)
-        .then(function(congresses){
-          $scope.congresses = congresses.data.result;
+        .then(function (res) {
+          $scope.congresses = res.data.result;
           CongressService.congresses = $scope.congresses;
-          $scope.hasCongresses = ($scope.congresses.length >= 0);
         }, function(err){
-          console.error(err);
+          show.error(err.message,"¡Error!");
         });
+
+      $scope.deleteCongress = function(id,index){
+        show.confirm("Este congreso se borrará","¿Está seguro?","Si, borrar",function(){
+          CongressService.delete(id,Session.id)
+            .then(function (ok) {
+              if (ok) {
+                $scope.congresses.splice(index, 1);
+                show.success("El congreso se ha borrado con exito","¡Congreso borrado!");
+              }
+            }, function (err) {
+              show.error(err.message,"¡Error!");
+            });
+        });
+      }
     };
 
-    var CongressCreateCtrl = function ($scope, Session, sponsors, participants, CongressService, $location) {
+    var CongressesCreateCtrl = function ($scope, Session, sponsors, participants, CongressService,$location) {
       $scope.sponsors = sponsors.data.result;
       $scope.participants = participants.data.result;
       $scope.congress = {
         session_id: Session.id
       };
-      $scope.addCongress = function(){
-        $scope.congress.patrocinio = 1;
+      $scope.addCongress = function(form){
+        if (!form.$valid) return;
         CongressService.create($scope.congress)
           .then(function(ok){
             if(ok){
               $scope.congress = {};
+              show.success("El congreso se ha creado con exito","¡Congreso creado!");
               $location.path( "/congresos" );
             }
           },function(err){
-            console.error(err);
+            show.error(err.message,"¡Error!");
           });
       };
     };
 
-    CongressCreateCtrl.resolve = {
+    var CongressesDetailsCtrl = function ($scope, Session, sponsors, participants, CongressService, $routeParams, $location  ) {
+      $scope.sponsors = sponsors.data.result;
+      $scope.participants = participants.data.result;
+      var id = $routeParams.id;
+      $scope.congress = CongressService.congresses.filter(function (el) {
+        return (el.id === +id);
+      })[0];
+      $scope.congress.session_id = Session.id;
+
+      $scope.addCongress = function(form){
+        if (!form.$valid) return;
+        CongressService.update($scope.congress)
+          .then(function (ok) {
+            if (ok) {
+              show.success("El congreso se ha actualizado con exito","¡Congreso actualizado!");
+              $location.path( "/congresos" );
+            }
+          }, function (err) {
+            show.error(err.message,"¡Error!");
+          });
+      }
+    };
+
+    CongressesCreateCtrl.resolve = {
       sponsors: function(SponsorService, Session){
         return SponsorService.getAll(Session.id);
       },
@@ -46,30 +83,25 @@ angular.module('diaApp')
     $routeProvider
       .when('/congresos', {
         templateUrl: 'components/congress/congresses.html',
-        controller: CongressListCtrl,
+        controller: CongressesListCtrl,
         data: {
           authorizedRoles: ["ADMIN"]
         }
       })
       .when('/congresos/crear', {
         templateUrl: 'components/congress/congress.html',
-        controller: CongressCreateCtrl,
-        resolve:CongressCreateCtrl.resolve,
+        controller: CongressesCreateCtrl,
+        resolve: CongressesCreateCtrl.resolve,
         data: {
           authorizedRoles: ["ADMIN"]
         }
       })
       .when('/congresos/:id', {
         templateUrl: 'components/congress/congress.html',
-        controllerAs: 'congressCtrl',
-        controller: function ($scope, CongressService, $routeParams) {
-          $scope.congress = {};
-
-          var id = $routeParams.id;
-          $scope.congress = CongressService.congresses.filter(function (el) {
-            return (el.id === +id);
-          })[0];
+        controller: CongressesDetailsCtrl,
+        resolve: CongressesCreateCtrl.resolve,
+        data: {
+          authorizedRoles: ["ADMIN"]
         }
       });
-
   });
