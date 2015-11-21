af5ac55297df1a189b097be24eec906315f9fff9
Luis Martinez <luizmartiinez@gmail.com>
2015-11-21 03:07:48 -0400

    Add toggle-button to publication

--- admin/app/app.module.js
+++ admin/app/app.module.js
@@ -1,16 +1,17 @@
 'use strict';
 
 angular.module('diaApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'angular-loading-bar',
-    'ngAnimate'
+  'ngAnimate',
+  'ngAria',
+  'ngCookies',
+  'ngMessages',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'angular-loading-bar',
+  'ngAnimate',
+  'ui.toggle'
 ]).config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
   cfpLoadingBarProvider.includeSpinner = false;
 }]);
