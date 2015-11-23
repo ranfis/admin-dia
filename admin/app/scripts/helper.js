6c866cb4bbe4fe9bad97e9e008577bec75448355
Ranfis S <ranfis.sh@gmail.com>
2015-11-22 22:18:48 -0400

    Remove ci 0.10 build

--- admin/app/scripts/helper.js
+++ admin/app/scripts/helper.js
@@ -2,26 +2,31 @@
 
 angular.module('diaApp')
   .service('Helper', function (MESSAGES) {
-  this.selectById = function(list,id){
-    var match = list.filter(function(e){
-      return (e.id === +id);
-    });
-    return match[0] || null;
-  };
-  this.getIDs = function(list){
-    var ids=[];
-    list.forEach(function (e) {
-      ids.push(e.id);
-    });
-    return ids;
-  };
-  this.checkResult = function(res){
-    if(res.data.msg !== MESSAGES.OK){
-      throw new Error(res.data.msg);
-    }
-    return res;
-  };
-  this.handleErrors = function(){
-    throw new Error(MESSAGES.ERROR.NO_INTERNET);
-  };
-});
+    this.selectById = function(list,id){
+      var match = list.filter(function(e){
+        return (e.id === +id);
+      });
+      return match[0] || null;
+    };
+    this.getIDs = function(list){
+      var ids=[];
+      list.forEach(function (e) {
+        ids.push(e.id);
+      });
+      return ids;
+    };
+    this.checkResult = function(res){
+      if(res.data.msg !== MESSAGES.OK){
+        throw new Error(res.data.msg);
+      }
+      return res;
+    };
+    this.handleErrors = function(){
+      throw new Error(MESSAGES.ERROR.NO_INTERNET);
+    };
+  })
+  .filter('siNo', function() {
+    return function(input) {
+      return input ? 'Sipi' : 'Nop';
+    };
+  });
