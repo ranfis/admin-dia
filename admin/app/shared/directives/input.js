d91b59e3c7baae5adfd7cb9b8e0d89e84887adba
Ranfis S <ranfis.sh@gmail.com>
2015-12-11 19:49:58 -0400

    Create a directive for chosen selects

--- admin/app/shared/directives/input.js
+++ admin/app/shared/directives/input.js
@@ -21,4 +21,25 @@ angular.module('diaApp')
       },
       templateUrl: 'app/shared/directives/input.html'
     };
+  })
+  .directive('uiChosen', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        uName: '@',
+        uDesc: '@',
+        uError: '@',
+        uMulti:'@',
+        uList: '=',
+        uListLabel: '@',
+        ngModel: '='
+      },
+      link: function(scope){
+        scope.uID = "input"+scope.uName;
+        scope.uError = "Este campo es requerido.";
+        scope.uDesc = scope.uDesc || scope.uName;
+        $("form").validator();
+      },
+      templateUrl: 'app/shared/directives/chosen.html'
+    };
   });
