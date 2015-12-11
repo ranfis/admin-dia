851e00dc40276af37e8b3a231c6869100bd4390a
Ranfis S <ranfis.sh@gmail.com>
2015-12-11 19:50:13 -0400

    Update project form

--- admin/app/components/project/project.js
+++ admin/app/components/project/project.js
@@ -4,14 +4,19 @@ angular.module('diaApp').service('ProjectService', new GenericService("project")
 
 angular.module('diaApp').config(
   new GenericController("ProjectService","Proyectos","project","projects",
-    [{service:"FundService",list:"funds"},{service:"ParticipantService",list:"participants"}],
+    [
+      {service:"ParticipantService",list:"participants"},
+      {service:"InstitutionService",list:"institutions"},
+      {service:"UnitService",list:"units"},
+      {service:"CurrencyService",list:"currencies"},
+      {service:"FundService",list:"funds"}
+    ],
     // After Fetch List
     function($scope){
       $scope["projects"].forEach(function(project){
         project.date_application = +project.date_application.slice(0,4);
         project.date_start = +project.date_start.slice(0,4);
       });
-      //console.log("Esto va a correr cuando hagan el list",$scope);
     },
     // After Fetch
     function ($scope,Helper) {
@@ -23,7 +28,5 @@ angular.module('diaApp').config(
     function($scope){
       $scope.project.date_application+="-00-00";
       $scope.project.date_start+="-00-00";
-
-      //console.log("Esto va a corer cuando hagan el save()",$scope);
     })
 );
