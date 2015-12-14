8064441d34dba409bcfc39911a990c5c7f52ffdc
Ranfis S <ranfis.sh@gmail.com>
2015-12-14 17:54:57 -0400

    Fix bug with currency/get

--- admin/app/components/currency/currency.js
+++ admin/app/components/currency/currency.js
@@ -1,12 +1,12 @@
 'use strict';
 
-angular.module('diaApp').service('CurrencyService', function ($http, WS, Helper){
+angular.module('diaApp').service('CurrencyService', function ($http, WS,ENV, Helper){
     this.list = function (sessionId) {
       var params = {
         params:{
           session_id: sessionId
         }
       };
-      return $http.get(WS.GET("currency"), params).then(Helper.checkResult,Helper.handleErrors);
+      return $http.get(ENV.WS_URL+WS.GET("currency"), params).then(Helper.checkResult,Helper.handleErrors);
     };
 });
