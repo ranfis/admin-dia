c921d9299e9a3669d5104a183bec881aaa1aa3f3
Luis Martinez <luizmartiinez@gmail.com>
2015-12-14 00:11:25 -0400

    Add all reports

--- admin/app/components/report/report.js
+++ admin/app/components/report/report.js
@@ -1,39 +1,63 @@
 'use strict';
 
 
-var ReportCtrl = function($scope, Session, WS, $rootScope){
-  $scope.report = {
-    year: {
-      earning:"",
-      overhead:""
-    }
-  };
+var ReportCtrl = function($scope, Session, WS, $rootScope, $sce, ParticipantService, Helper){
   $rootScope.title = "Reportes";
   $rootScope.nav = "Reportes";
+  $scope.report = {
+    participant: 0
+  };
   var encodedSession = btoa(Session.id);
-  $scope.save = function(index, form) {
+
+
+  ParticipantService.list(Session.id)
+    .then(function(res){
+      $scope.participants = res.data.result;
+    });
+
+  $scope.save = function(index, form, project) {
     var location_url = "";
     switch(index) {
       case 1: {
-        location_url = WS.REPORT.EARNINGS+encodedSession;
+        location_url = WS.REPORT.EARNINGS.SIMPLE;
         break;
       }
       case 2: {
-        location_url = WS.REPORT.OVERHEAD+encodedSession;
+        location_url = WS.REPORT.EARNINGS.OVERHEAD;
         break;
       }
       case 3: {
-        location_url = WS.REPORT.EARNINGS+encodedSession+"?year="+$scope.report.year.earning;
+        location_url = WS.REPORT.EARNINGS.TOTALBOTH;
         break;
       }
       case 4: {
-        location_url = WS.REPORT.OVERHEAD+encodedSession+"?year="+$scope.report.year.overhead;
+        location_url = WS.REPORT.PROJECTS.QUANTITY;
+        break;
+      }
+      case 5: {
+        location_url = WS.REPORT.PROJECTS.SIMPLE;
+        break;
+      }
+      case 6: {
+        location_url = WS.REPORT.CONGRESS;
+        break;
+      }
+      case 7: {
+        location_url = WS.REPORT.PUBLICATION;
+        break;
+      }
+      case 8: {
+        location_url = WS.REPORT.ANNUAL;
         break;
       }
     }
     if (!form.$valid){return;}
-    window.location.href = location_url;
+    //window.location.href = location_url;
+
+      $scope.report_action = $sce.trustAsResourceUrl(location_url+encodedSession);
+
   }
+
 };
 angular.module('diaApp').controller(ReportCtrl)
   .config(function ($routeProvider, PATH, USER_ROLES) {
