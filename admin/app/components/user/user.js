1ea8753847159de629617289e13879be939a3928
Ranfis S <ranfis.sh@gmail.com>
2016-02-26 01:45:46 -0400

    CleanUp

--- admin/app/components/user/user.js
+++ admin/app/components/user/user.js
@@ -3,11 +3,5 @@
 angular.module('diaApp').service('UserService', new GenericService("user"));
 
 angular.module('diaApp').config(
-  new UserController("UserService", "Usuario", "user", "users",
-    null,
-    null,
-    function ($scope,Helper) {
-
-    },
-    null)
+  new UserController("UserService", "Usuario", "user", "users")
 );
