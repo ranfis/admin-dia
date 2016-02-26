903a498cff13233134469ec496038e1abca0dde1
Luis Martinez <luizmartiinez@gmail.com>
2016-02-26 02:58:05 -0400

    Add filter for roles

--- admin/app/scripts/filters.js
+++ admin/app/scripts/filters.js
@@ -4,4 +4,15 @@ angular.module('diaAppFilters', []).filter('sino', function() {
   return function(input) {
     return input ? 'Si' : 'No';
   };
-});
+})
+  .filter('roles', function() {
+    return function(input) {
+      if (input=="REPORT") {
+        return "Reportes";
+      } else if (input=="ADMIN") {
+        return "Administrador";
+      } else if (input=="SUPER-ADMIN") {
+        return "Super-Administrador";
+      }
+    }
+  });
